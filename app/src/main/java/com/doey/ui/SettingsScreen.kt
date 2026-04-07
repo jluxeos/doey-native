b2328aab19bc5aca909775542ef1a38f85acaaea
jluxeos <eduardolopeztexis@gmail.com>
2026-04-06 21:40:50 -0600

    Actualizar SettingsScreen.kt

--- app/src/main/java/com/doey/ui/SettingsScreen.kt
+++ app/src/main/java/com/doey/ui/SettingsScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.doey.DoeyApplication
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
